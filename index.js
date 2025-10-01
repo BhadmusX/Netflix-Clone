0fba05d8f149d2afae0699e4847ba26ff5e376e7
BhadmusX <bhadmusalameen052007@gmail.com>
2025-10-01 12:46:28 +0100

    fixed a null error in the index.js file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
- const movies = document.querySelector('.movies');
-const leftBtn = document.getElementById('leftBtn');
-const rightBtn = document.getElementById('rightBtn');
 
+document.addEventListener(
+  "DOMContentLoaded", () => {
+    const leftBtn = document.getElementById('leftBtn');
+const rightBtn = document.getElementById('rightBtn');
+const movies = document.querySelector('.movies');
 leftBtn.addEventListener('click', () => {
   movies.scrollBy({ left: -300, behavior: 'smooth' });
 });
-
 rightBtn.addEventListener('click', () => {
   movies.scrollBy({ left: 300, behavior: 'smooth' });
 });
+  });
